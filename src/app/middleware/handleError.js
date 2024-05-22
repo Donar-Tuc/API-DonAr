fa4193a9d4166b0007c1af9a54a85b01d152a1f2
Donar-Tuc <167128188+Donar-Tuc@users.noreply.github.com>
2024-05-22 19:45:17 -0300

    Update handleError.js

--- src/app/middleware/handleError.js
+++ src/app/middleware/handleError.js
@@ -1,5 +1,5 @@
 const handleError = ((err, req, res, next) => {
-    console.err("\n\n\n---- ERROR ----\n\n\n", err); // Log del error
+    console.error("\n\n\n---- ERROR ----\n\n\n", err); // Log del error
     res.status(err.status || 500).json({ message: err.message || 'Error interno del servidor' });
   });
   
