fca60aedea7680a343886a39c0978983b15117f3
Donar-Tuc <167128188+Donar-Tuc@users.noreply.github.com>
2024-05-22 21:21:32 -0300

    Update mongo.js

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 const DB_URI = process.env.DB_URI;
+let cachedDb = null;
 
 
 async function dbConnect() {
+    if (cachedDb) {
+        return cachedDb;
+    }
     try {
-      await mongoose.connect(DB_URI, {});
+      var connection = await mongoose.connect(DB_URI, {
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+      });
       console.log('Conexión a la base de datos exitosa');
+      cachedDb = connection;
+      return connection;
     } catch (error) {
       console.error('Error al conectar a la base de datos', error);
     }
