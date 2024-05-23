201fd76ce06b055321f1143278d24e0626908e46
Donar-Tuc <167128188+Donar-Tuc@users.noreply.github.com>
2024-05-23 16:47:18 -0300

    Update mongo.js
    Se agregó throw error

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -2,22 +2,25 @@ const mongoose = require("mongoose");
 const DB_URI = process.env.DB_URI;
 let cachedDb = null;
 
-
 async function dbConnect() {
     if (cachedDb) {
+        console.log("Usando conexión de base de datos en caché");
         return cachedDb;
     }
+
     try {
-      var connection = await mongoose.connect(DB_URI, {
-        serverSelectionTimeoutMS: 5000,
-        socketTimeoutMS: 45000,
-      });
-      console.log('Conexión a la base de datos exitosa');
-      cachedDb = connection;
-      return connection;
+        console.log("Intentando conectar a la base de datos...");
+        var connection = await mongoose.connect(DB_URI, {
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        });
+        console.log('Conexión a la base de datos exitosa');
+        cachedDb = connection;
+        return connection;
     } catch (error) {
-      console.error('Error al conectar a la base de datos', error);
+        console.error('Error al conectar a la base de datos', error);
+        throw error;
     }
-  }
+}
 
 module.exports = { dbConnect };
