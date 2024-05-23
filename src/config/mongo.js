868f24b12691d5fe0b8c0b573f0db3b37b1f36a0
Donar-Tuc <167128188+Donar-Tuc@users.noreply.github.com>
2024-05-23 17:10:26 -0300

    Update mongo.js
    logs en caso de no haber URI

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const DB_URI = process.env.DB_URI;
+
 let cachedDb = null;
 
 async function dbConnect() {
@@ -8,17 +9,23 @@ async function dbConnect() {
         return cachedDb;
     }
 
+    if (!DB_URI) {
+        throw new Error("DB_URI no está definido en las variables de entorno");
+    }
+
     try {
         console.log("Intentando conectar a la base de datos...");
-        var connection = await mongoose.connect(DB_URI, {
+        const connection = await mongoose.connect(DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 5000,
             socketTimeoutMS: 45000,
         });
-        console.log('Conexión a la base de datos exitosa');
+        console.log("Conexión a la base de datos exitosa");
         cachedDb = connection;
         return connection;
     } catch (error) {
-        console.error('Error al conectar a la base de datos', error);
+        console.error("Error al conectar a la base de datos", error);
         throw error;
     }
 }
