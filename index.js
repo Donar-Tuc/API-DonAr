bd3fdab6495254c2f846c10efb56adae3252c7cb
Donar-Tuc <167128188+Donar-Tuc@users.noreply.github.com>
2024-05-22 21:00:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,24 +13,9 @@ app.use(handleError);
 
 const PORT = process.env.PORT || 3001;
 
-async function startServer() {
-    try {
-        await dbConnect();
-        console.log("**** CONEXION ESTABLECIDA ****");
-
-        const server = app.listen(PORT, () => {
-            console.log("Servidor escuchando en el puerto " + PORT);
-        });
-
-        server.timeout = 30000; // Aumenta el tiempo de espera a 30 segundos
-    } catch (error) {
-        console.error("Error al conectar a la base de datos:", error);
-        process.exit(1);
-    }
-}
-
-if (process.env.NODE_ENV !== 'production') {
-    startServer();
-}
+app.listen(PORT, async() => {
+    await dbConnect();
+    console.log("Servidor escuchando en el puerto " + PORT);
+});
 
 module.exports = app;
