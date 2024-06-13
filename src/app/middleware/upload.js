0896c86f109ed5ee8b8d167e823b8fb63a059b42
tizianocarranza <118763080+tizianocarranza@users.noreply.github.com>
2024-06-13 20:10:51 -0300

    Update upload.js to use temporal directory for vercel

--- src/app/middleware/upload.js
+++ src/app/middleware/upload.js
@@ -3,7 +3,7 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./src/app/uploads")
+        cb(null, os.tmpdir());
     },
     filename: function (req, file, cb) {
         let ext = path.extname(file.originalname)
