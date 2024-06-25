9491d841ac39047c29ed6fcd8a59705e3e147c17
tizianocarranza <tizianocarranza0910@gmail.com>
2024-06-25 20:24:23 -0300

    DBConnect  mods

--- src/app/controllers/upload.js
+++ src/app/controllers/upload.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose');
-const { GridFSBucket, ObjectId } = require("mongodb");
+const { GridFSBucket, ObjectId } = require('mongodb');
 
 const getFileFromGridFS = (req, res, next) => {
+    console.log('Iniciando getFileFromGridFS');
+    
     const db = mongoose.connection.db;
+    console.log('Conexión a la base de datos establecida');
 
     const bucket = new GridFSBucket(db, { bucketName: 'uploads' });
-
+    console.log('Bucket de GridFS creado');
 
     const fileId = ObjectId(req.params.id);
+    console.log('ID del archivo:', fileId);
 
     bucket.find({ _id: fileId }).toArray((err, files) => {
         if (err) {
+            console.error('Error al buscar archivos en GridFS:', err);
             return next(err);
         }
 
         if (!files || files.length === 0) {
+            console.log('Archivo no encontrado en GridFS');
             return res.status(404).send('No file found');
         }
+
+        console.log('Archivo encontrado en GridFS:', files[0]);
 
         res.set('Content-Type', files[0].contentType);
         res.set('Content-Disposition', `attachment; filename="${files[0].filename}"`);
@@ -24,10 +32,12 @@ const getFileFromGridFS = (req, res, next) => {
         const downloadStream = bucket.openDownloadStream(fileId);
 
         downloadStream.on('error', (error) => {
+            console.error('Error al abrir flujo de descarga:', error);
             next(error);
         });
 
         downloadStream.on('end', () => {
+            console.log('Descarga del archivo completada');
             res.end();
         });
 
