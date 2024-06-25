0773327cdb76329ff114ef89b1899f396f5b6199
tizianocarranza <tizianocarranza0910@gmail.com>
2024-06-25 19:19:33 -0300

    Multer mods

--- index.js
+++ index.js
@@ -42,16 +42,16 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnect } = require('./src/config/mongo');
 const { handleError } = require('./src/app/middleware/handleError');
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 const { GridFSBucket, ObjectId } = require('mongodb');
-const { upload, uploadFileToGridFS } = require('./src/app/middleware/upload'); // Asegúrate de usar la ruta correcta a tu archivo uploadConfig.js
+const { upload, uploadFileToGridFS } = require('./src/app/middleware/upload'); */ // Asegúrate de usar la ruta correcta a tu archivo uploadConfig.js
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/', require('./src/app/routes'));
-app.use('/upload/file/:id', express.static('uploads/logosFundaciones')); // Ajusta la ruta según donde deseas servir los archivos estáticos
+/* app.use('/upload/file/:id', express.static('uploads/logosFundaciones')); // Ajusta la ruta según donde deseas servir los archivos estáticos */
 
 app.use(handleError);
 
