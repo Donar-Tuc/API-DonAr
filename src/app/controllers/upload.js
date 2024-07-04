eb2a32e1911f3b9199f21f2eaeb329c11302caa4
tizianocarranza <tizianocarranza0910@gmail.com>
2024-07-04 19:33:06 -0300

    API old version

--- src/app/controllers/upload.js
+++ src/app/controllers/upload.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const getFileFromGridFS = async (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
