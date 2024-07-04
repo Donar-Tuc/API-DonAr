65c3132e8b95bb0ea1adec3a1fda196023fe7299
tizianocarranza <tizianocarranza0910@gmail.com>
2024-07-04 19:30:14 -0300

    API old version

--- src/app/controllers/upload.js
+++ src/app/controllers/upload.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
-const { GridFSBucket, ObjectId } = require('mongodb');
-
 const getFileFromGridFS = async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
     try {
         console.log('Iniciando getFileFromGridFS');
 
-        const db = mongoose.connection.db;
-        
-        // Verificar si la conexión a la base de datos está establecida
-        if (!db) {
+        if (!mongoose.connection.db) {
             throw new Error('No se pudo establecer la conexión a la base de datos');
         }
 
+        const db = mongoose.connection.db;
         console.log('Conexión a la base de datos establecida');
 
         const bucket = new GridFSBucket(db, { bucketName: 'uploads' });
@@ -36,27 +35,22 @@ const getFileFromGridFS = async (req, res, next) => {
 
         downloadStream.on('error', (error) => {
             console.error('Error al abrir flujo de descarga:', error);
-            if (!res.headersSent) {
-                return res.status(500).send({ error: 'Error al abrir flujo de descarga' });
-            }
+            res.status(500).send({ error: 'Error al abrir flujo de descarga' });
+            next(error);
         });
 
         downloadStream.on('end', () => {
             console.log('Descarga del archivo completada');
-            if (!res.headersSent) {
-                res.end();
-            }
+            res.end();
         });
 
         downloadStream.pipe(res);
 
     } catch (error) {
         console.error('Error en getFileFromGridFS:', error);
-        if (!res.headersSent) {
-            return res.status(500).send({ error: 'Error en getFileFromGridFS' });
-        }
-        next(error); // Solo llamar a next si no se ha enviado una respuesta
+        res.status(500).send({ error: 'Error en getFileFromGridFS' });
+        next(error);
     }
 };
 
-module.exports = { getFileFromGridFS };
+module.exports = {getFileFromGridFS}
