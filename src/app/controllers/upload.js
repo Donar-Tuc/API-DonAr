f28ecc70fbb870189f5d8ef6ca6dfccb263ddf05
tizianocarranza <118763080+tizianocarranza@users.noreply.github.com>
2024-07-06 16:30:18 -0300

    Update upload.js

--- src/app/controllers/upload.js
+++ src/app/controllers/upload.js
@@ -7,29 +7,23 @@ const getFileFromGridFS = async (req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     
     try {
-        console.log('Iniciando getFileFromGridFS');
 
         if (!mongoose.connection.db) {
             throw new Error('No se pudo establecer la conexión a la base de datos');
         }
 
         const db = mongoose.connection.db;
-        console.log('Conexión a la base de datos establecida');
 
         const bucket = new GridFSBucket(db, { bucketName: 'uploads' });
-        console.log('Bucket de GridFS creado');
 
         const fileId = new ObjectId(req.params.id);
-        console.log('ID del archivo:', fileId);
 
         const files = await bucket.find({ _id: fileId }).toArray();
 
         if (!files || files.length === 0) {
-            console.log('Archivo no encontrado en GridFS');
             return res.status(404).send('No se encontró el archivo');
         }
 
-        console.log('Archivo encontrado en GridFS:', files[0]);
 
         res.set('Content-Type', files[0].contentType);
         res.set('Content-Disposition', `inline; filename="${files[0].filename}"`);
@@ -43,7 +37,6 @@ const getFileFromGridFS = async (req, res, next) => {
         });
 
         downloadStream.on('end', () => {
-            console.log('Descarga del archivo completada');
             res.end();
         });
 
