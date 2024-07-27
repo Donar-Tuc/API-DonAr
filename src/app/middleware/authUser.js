93310c079d8250af74aa419ceb31546098722219
tizianocarranza <tizianocarranza0910@gmail.com>
2024-07-27 19:16:02 -0300

    Auth mod

--- src/app/middleware/authUser.js
+++ src/app/middleware/authUser.js
@@ -1,18 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 const authUser = async (req, res, next) => {
-    const authorizationToken = req.headers.authorization;
-    try 
-    {
-        const decodedToken = jwt.verify(authorizationToken, process.env.JWT_PASSWORD);
-        req.user = decodedToken; 
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+        return res.status(401).json({ message: "Authorization header missing" });
+    }
+
+    const token = authorizationHeader.split(" ")[1]; // Assuming "Bearer <token>"
+
+    if (!token) {
+        return res.status(401).json({ message: "Token missing" });
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_PASSWORD);
+        req.user = decodedToken;
 
         next();
-    } 
-    catch (error) 
-    {
-        next(error);
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token", error: error.message });
     }
-}
+};
 
-module.exports= { authUser }
+module.exports = { authUser };
